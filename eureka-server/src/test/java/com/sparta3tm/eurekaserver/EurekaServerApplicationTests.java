f4b5489bb4db89673c8473f5fa079517d46b69ff
송형근 <soo1095@naver.com>
2024-09-06 17:29:29 +0900

    feat: Eureka Server 환경 세팅

---
+++ eureka-server/src/test/java/com/sparta3tm/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta3tm.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
