5c16db82f750743b365b8b2e31324b9ce50ee305
CZ-punk <cnhhw0408@gmail.com>
2024-09-09 11:30:36 +0900

    Hub CRUD + test code 구현

---
+++ hub-server/src/main/java/com/sparta3tm/hubserver/HubServerApplication.java
@@ -0,0 +1,13 @@
+package com.sparta3tm.hubserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HubServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HubServerApplication.class, args);
+    }
+
+}
