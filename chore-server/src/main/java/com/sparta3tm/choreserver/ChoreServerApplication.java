9aa42963177ce6c10770a69221f1c4e85fa2ebab
송형근 <soo1095@naver.com>
2024-09-11 17:25:31 +0900

    feat: Chore Server 환경 설정

---
+++ chore-server/src/main/java/com/sparta3tm/choreserver/ChoreServerApplication.java
@@ -0,0 +1,13 @@
+package com.sparta3tm.choreserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication()
+public class ChoreServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChoreServerApplication.class, args);
+    }
+
+}
---
+++ chore-server/src/test/java/com/sparta3tm/choreserver/ChoreServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta3tm.choreserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChoreServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
